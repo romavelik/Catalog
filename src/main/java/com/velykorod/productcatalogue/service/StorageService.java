c7447715018a87f4fefa55ebdaf16db94273df26
Roman Velikorod <romavelik@gmail.com>
2019-12-03 21:04:20 +0200

    added storage

--- src/main/java/com/velykorod/productcatalogue/service/StorageService.java
+++ src/main/java/com/velykorod/productcatalogue/service/StorageService.java
@@ -1,11 +1,14 @@
 package com.velykorod.productcatalogue.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.Resource;
+import java.nio.file.Path;
 
 public interface StorageService {
     public void store(MultipartFile file);
 
     public Resource loadAsResource(String fileName);
+
+    public Path load(String fileName);
 }
